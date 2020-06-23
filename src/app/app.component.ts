7f9931c1e3368af1d5cba96ec2f3454bc70a91da
Hamid <asheikh@aliviaanalytics.com>
2020-06-23 22:07:00 +0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {LazyLoadEvent} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'primeng-infinite-scroll';
+export class AppComponent implements OnInit {
+
+  data: any[];
+  cols: any[];
+
+  ngOnInit() {
+    for (let i = 0; i < 200; i++) {
+      this.data.push({ id: i, value: "Value " + i })
+    }
+    this.cols = [
+      { field: 'id', header: 'ID' },
+      { field: 'value', header: 'Value' }
+    ];
+  }
+
+  loadCarsLazy(event: LazyLoadEvent) {   
+    console.log(event);
+        
+    //simulate remote connection with a timeout 
+}
+
+
 }
